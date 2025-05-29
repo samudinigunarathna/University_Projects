ae0dbc46fc46ebe9108ee70595bcfe7745a5a336
Samudini Gunarathna <samudinigunarathna@gmail.com>
2025-05-29 08:29:05 +0000

    Updated App Class

--- Lab_Sheet_01/Item.java
+++ Lab_Sheet_01/Item.java
@@ -18,12 +18,23 @@ class Customer{
 }
 
 class DataBase{
+    ArrayList<Item>Items=new ArrayList<>();
+    ArrayList<Customer>Customers=new ArrayList<>();
 
 }
 
 class App{
     public static void main(String[] args){
         System.out.println("-----Inventory Management-----");
-
+        System.out.println("1.Add Item");
+        System.out.println("2.Remove Item");
+        System.out.println("3.Update Item Quantity");
+        System.out.println("4.List All Items");
+        System.out.println("5.Add Customer");
+        System.out.println("List All Customers");
+        System.out.println("Search Item by Name");
+        System.out.println("0.Exit");
+        System.out.println("----------------------------");
+        System.out.println("Enter Your Choice");
     }
 }
